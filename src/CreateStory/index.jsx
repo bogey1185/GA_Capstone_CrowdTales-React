87ea78836320c8b0d363e0fec7f256f8815c7d6f
David Bogaerts <dbogaerts@gmail.com>
2019-03-12 10:37:18 -0500

    style create page

--- src/CreateStory/index.jsx
+++ src/CreateStory/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import history from '../history';
+import './index.css'
 
 class CreateStory extends Component {
   
@@ -28,58 +29,68 @@ class CreateStory extends Component {
   render() {
 
     return (
-      <div>
+      <div className="outer-create">
+
         <h3>Start a new tale:</h3>
-        <form onSubmit={this.props.handleCreate.bind(null, this.state)}>
-          <label>Title:</label>
-            <input 
-              type="text" 
-              name="title" 
-              required
-              value={this.state.title} 
-              onChange={this.handleChange}
-              placeholder="Your store title..."
-            /><br />
-          <label>Genre: </label>
-            <select
-              name="genre"
-              value={this.state.genre}
-              onChange={this.handleChange}
-              required
-            >
-              <option value=" "> </option>
-              <option value="Action/Adventure">Action/Adventure</option>
-              <option value="Children's Literature">Children's Literature</option>
-              <option value="Coming-of-Age">Coming-of-Age</option>
-              <option value="Crime">Crime</option>
-              <option value="Drama">Drama</option>
-              <option value="Fairytale">Fairytale</option>
-              <option value="Fantasy">Fantasy</option>
-              <option value="Historical Fiction">Historical Fiction</option>
-              <option value="Horror">Horror</option>
-              <option value="Mystery">Mystery</option>
-              <option value="Romance">Romance</option>
-              <option value="Satire">Satire</option>
-              <option value="Science Fiction">Science Fiction</option>
-              <option value="Short Story">Short Story</option>
-              <option value="Suspence">Suspence</option>
-              <option value="Thriller">Thriller</option>
-              <option value="Young Adult">Young Adult</option>
-            </select><br />
-          <label>Writing Prompt: </label>
-            <textarea 
-              type="text" 
-              name="text"
-              rows="10" 
-              cols="50"
-              required
-              value={this.state.text}
-              onChange={this.handleChange}
-              placeholder="Enter your writing prompt here..."
-            >
-            </textarea><br />
-          <button type="submit">Submit</button>
-        </form>
+        <div className="inner-create">
+          <div className="inner-labels">
+            <label>Title:</label>
+            <label>Genre: </label>
+            <label>Writing Prompt: </label>
+          </div>
+          <div className="inner-form">
+            <form id="create" onSubmit={this.props.handleCreate.bind(null, this.state)}>
+              <input 
+                type="text" 
+                name="title"
+                id="createtitle" 
+                required
+                value={this.state.title} 
+                onChange={this.handleChange}
+                placeholder="Your store title..."
+              />
+              <select
+                name="genre"
+                id="genre"
+                value={this.state.genre}
+                onChange={this.handleChange}
+                required
+              >
+                <option value=" "> </option>
+                <option value="Action/Adventure">Action/Adventure</option>
+                <option value="Children's Literature">Children's Literature</option>
+                <option value="Coming-of-Age">Coming-of-Age</option>
+                <option value="Crime">Crime</option>
+                <option value="Drama">Drama</option>
+                <option value="Fairytale">Fairytale</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Historical Fiction">Historical Fiction</option>
+                <option value="Horror">Horror</option>
+                <option value="Mystery">Mystery</option>
+                <option value="Romance">Romance</option>
+                <option value="Satire">Satire</option>
+                <option value="Science Fiction">Science Fiction</option>
+                <option value="Short Story">Short Story</option>
+                <option value="Suspence">Suspence</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Young Adult">Young Adult</option>
+              </select>
+              <textarea 
+                id="createtext"
+                type="text" 
+                name="text"
+                rows="10" 
+                cols="50"
+                required
+                value={this.state.text}
+                onChange={this.handleChange}
+                placeholder="Enter your writing prompt here..."
+              >
+              </textarea>
+            </form>
+              <button id="createbtn" type="submit" form="create">Submit</button>
+          </div>
+        </div>
       </div>
     )
   }
